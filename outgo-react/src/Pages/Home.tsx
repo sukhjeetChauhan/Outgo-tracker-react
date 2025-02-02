b201676532719807510464b65d10edd67dc206a1
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2025-02-02 22:02:40 +0530

    added home page layout

--- outgo-react/src/Pages/Home.tsx
+++ outgo-react/src/Pages/Home.tsx
@@ -11,7 +11,7 @@ import LogoutButton from '../components/LogoutButton'
 export default function Home() {
   const { instance, accounts } = useMsal()
   const dispatch = useDispatch<AppDispatch>()
-  const { id, firstName, lastName } = useSelector(
+  const { id, firstName, lastName, status } = useSelector(
     (state: RootState) => state.user
   )
 
@@ -19,27 +19,32 @@ export default function Home() {
     dispatch(fetchUser({ accounts, instance }))
   }, [dispatch, accounts, instance])
 
-  if (status === 'loading') return <p>Loading user...</p>
-  if (status === 'failed') return <p>Error fetching user</p>
+  // if (status === 'loading') return <p>Loading user...</p>
+  // if (status === 'failed') return <p>Error fetching user</p>
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-4xl text-center text-blue-500 font-bold mt-10">
-        Welcome to Outgo
-      </h1>
-      {accounts.length > 0 ? (
+    <div className="w-full min-h-screen bg-gray-200">
+      <div className="flex w-full mx-auto">
         <div>
-          <div>
-            <p>{`Id: ${id}`}</p>
-            <p>Welcome, {`${firstName} ${lastName}`}</p>
+          <div className="bg-teal-300 w-72 h-screen flex flex-col items-center ">
+            <div className="border-b-2 border-gray-200 w-full h-24">
+              <h2 className="text-white font-semibold text-2xl flex items-center justify-center h-full">
+                Outgo Tracker
+              </h2>
+            </div>
           </div>
-          <LogoutButton />
         </div>
-      ) : (
-        <LoginButton />
-      )}
-      <PasswordReset />
-      {/* <button onClick={() => GetUserInfo(accounts, instance)}>Click</button> */}
+        <div className="min-h-screen flex-1 flex flex-col items-center p-4">
+          <div className="border-b-2 border-gray-200 w-full h-24"></div>
+          <div className="border-b-2 border-gray-200 w-full h-24 rounded bg-teal-100 mb-2"></div>
+          <div className="w-full flex-1 grid grid-cols-[1fr_1fr] gap-4">
+            <div className="bg-white rounded"></div>
+            <div className="bg-white rounded"></div>
+            <div className="bg-white rounded"></div>
+            <div className="bg-white rounded"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
