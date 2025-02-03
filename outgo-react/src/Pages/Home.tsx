cd19419f4ee80c9c75723f69c970f76f187d909b
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2025-02-03 22:27:07 +0530

    make sidebar menu component

--- outgo-react/src/Pages/Home.tsx
+++ outgo-react/src/Pages/Home.tsx
@@ -4,8 +4,9 @@ import LoginButton from '../components/LoginButton'
 import PasswordReset from '../components/PasswordReset'
 import { fetchUser } from '../Redux/Slices/userSlice'
 import { RootState, AppDispatch } from '../Redux/store'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import LogoutButton from '../components/LogoutButton'
+import Menubar from '../components/Menubar'
 // import fetchData from '../assets/apis/fetchapi'
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
   const { id, firstName, lastName, status } = useSelector(
     (state: RootState) => state.user
   )
+  const [retractMenu, setRetractMenu] = useState(false)
 
   useEffect(() => {
     dispatch(fetchUser({ accounts, instance }))
@@ -26,11 +28,20 @@ export default function Home() {
     <div className="w-full min-h-screen bg-gray-200">
       <div className="flex w-full mx-auto">
         <div>
-          <div className="bg-teal-300 w-72 h-screen flex flex-col items-center ">
-            <div className="border-b-2 border-gray-200 w-full h-24">
+          <div className="bg-teal-300 h-screen flex flex-col items-center ">
+            <div className="h-24">
               <h2 className="text-white font-semibold text-2xl flex items-center justify-center h-full">
-                Outgo Tracker
+                Outgo
               </h2>
+            </div>
+            <div className="flex-1 border-t-2 border-gray-200 relative">
+              <Menubar retractMenu={retractMenu} />
+              <button
+                className="px-[7px] py-[1px] rounded-full bg-white text-teal-300 text-3xl font-bold absolute -right-4 -top-4 text-center"
+                onClick={() => setRetractMenu(!retractMenu)}
+              >
+                {retractMenu ? '>' : '<'}
+              </button>
             </div>
           </div>
         </div>
---
+++ outgo-react/src/components/Menubar.tsx
@@ -0,0 +1,31 @@
+const menubarItems = [
+  { name: 'Dashboard', path: '/', icon: 'X' },
+  { name: 'Project', path: '/project', icon: 'X' },
+  { name: 'Expense', path: '/expense', icon: 'X' },
+  { name: 'Income', path: '/income', icon: 'X' },
+]
+
+export default function Menubar({ retractMenu }: { retractMenu: boolean }) {
+  return (
+    <div className="flex flex-col items-center my-4 gap-4">
+      {menubarItems.map((item, index) => {
+        return (
+          <a href={item.path} key={index}>
+            <div className="flex items-center py-2 px-8">
+              <div className="px-4">
+                <span>{item.icon}</span>
+              </div>
+              <div
+                className={`px-4 ${
+                  retractMenu ? 'w-0' : 'w-44'
+                } transition-all duration-500 ease-in-out overflow-hidden`}
+              >
+                <p className="text-white font-semibold text-xl">{item.name}</p>
+              </div>
+            </div>
+          </a>
+        )
+      })}
+    </div>
+  )
+}
