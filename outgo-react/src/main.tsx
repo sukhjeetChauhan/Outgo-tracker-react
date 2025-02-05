57f406473f3fe188e87a58ded7e5c9a83fb42c68
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2025-02-05 22:37:52 +0530

    working on api functions

--- outgo-react/src/main.tsx
+++ outgo-react/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { msalInstance } from './authentication/authConfig.ts'
@@ -9,16 +9,20 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './Redux/store.ts'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <MsalProvider instance={msalInstance}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <App />
-          </PersistGate>
-        </Provider>
-      </MsalProvider>
+      <QueryClientProvider client={queryClient}>
+        <MsalProvider instance={msalInstance}>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <App />
+            </PersistGate>
+          </Provider>
+        </MsalProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 )
