dad2ad6dbd052d6ac4309b6ed1704ac6e92b7c15
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2025-02-08 21:06:08 +0530

    add ProjectUser apis

---
+++ outgo-react/src/apis/ProjectUser/ProjectUserRepository.ts
@@ -0,0 +1,62 @@
+import axios from 'axios'
+import { Role } from '../../Types/enums'
+
+const backendApiUrl = 'http://localhost:5298/api'
+
+export interface ProjectUser {
+  userId: string
+  projectId: number
+  role: Role
+}
+
+export class ProjectUserRepository {
+  static async getUserByProjectId(projectId: number) {
+    const { data } = await axios.get(
+      `${backendApiUrl}/ProjectUser/GetUsersByProjectId/${projectId}`
+    )
+    return data
+  }
+
+  static async getProjectByUserId(userId: string) {
+    const { data } = await axios.get(
+      `${backendApiUrl}/ProjectUser/GetProjectsByUserId/${userId}`
+    )
+    return data
+  }
+
+  static async getById(id: number) {
+    const { data } = await axios.get(`${backendApiUrl}/ProjectUser/${id}`)
+    return data
+  }
+
+  static async create(projectUser: ProjectUser) {
+    const { data } = await axios.post(
+      `${backendApiUrl}/ProjectUser`,
+      projectUser
+    )
+    return data
+  }
+
+  static async update(id: number, projectUser: ProjectUser) {
+    try {
+      const { data } = await axios.put(
+        `${backendApiUrl}/ProjectUser/${id}`,
+        projectUser
+      )
+      return data
+    } catch (error) {
+      console.error('Error updating project user:', error)
+      throw error
+    }
+  }
+
+  static async delete(id: number) {
+    try {
+      const { data } = await axios.delete(`${backendApiUrl}/ProjectUser/${id}`)
+      return data
+    } catch (error) {
+      console.error('Error deleting project user:', error)
+      throw error
+    }
+  }
+}
---
+++ outgo-react/src/apis/ProjectUser/useProjectUsers.ts
@@ -0,0 +1,50 @@
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { ProjectUser, ProjectUserRepository } from './ProjectUserRepository'
+
+export const useGetUsersByProjectId = (projectId: number) => {
+  return useQuery({
+    queryKey: ['projectUser', 'users', projectId],
+    queryFn: () => ProjectUserRepository.getUserByProjectId(projectId),
+  })
+}
+export const useGetProjectsByUserId = (userId: string) => {
+  return useQuery({
+    queryKey: ['projectUser', 'projects', userId],
+    queryFn: () => ProjectUserRepository.getProjectByUserId(userId),
+  })
+}
+export const useCreateProjectUser = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: ProjectUserRepository.create,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['projectUser'] })
+    },
+  })
+}
+
+export const useUpdateProjectUser = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: ({
+      id,
+      projectUser,
+    }: {
+      id: number
+      projectUser: ProjectUser
+    }) => ProjectUserRepository.update(id, projectUser),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['projectUser'] })
+    },
+  })
+}
+
+export const useDeleteProjectUser = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: ProjectUserRepository.delete,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['projectUser'] })
+    },
+  })
+}
