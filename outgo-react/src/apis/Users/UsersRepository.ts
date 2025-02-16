f48f4415e75d8be164e7b8d6676ccf8918ce0295
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2025-02-16 22:13:57 +0530

    add UserRepository

---
+++ outgo-react/src/apis/Users/UsersRepository.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const backendApiUrl = 'http://localhost:5298/api'
+
+export interface User {
+  id: string
+  firstName: string
+  lastName: string
+  email?: string
+  phoneNumber?: number
+  defaultProjectId: number
+}
+
+export class UsersRepository {
+  static async getById(id: string) {
+    const { data } = await axios.get(`${backendApiUrl}/User/${id}`)
+    return data
+  }
+
+  static async create(user: User) {
+    const { data } = await axios.post(`${backendApiUrl}/User`, user)
+    return data
+  }
+
+  static async Update(id: string, user: User) {
+    const { data } = await axios.put(`${backendApiUrl}/User/${id}`, user)
+    return data
+  }
+
+  static async delete(id: string) {
+    const { data } = await axios.delete(`${backendApiUrl}/User/${id}`)
+    return data
+  }
+}
