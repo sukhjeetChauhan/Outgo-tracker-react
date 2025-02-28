383c9513d749f4bdd7ba4bb1298a2a823ff70473
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2025-02-28 07:17:03 +0530

    design dataTable

--- outgo-react/src/Pages/Expense.tsx
+++ outgo-react/src/Pages/Expense.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux'
 import { RootState } from '../Redux/store'
 import { useWeeklyExpenses } from '../apis/Expenses/useExpenses'
-import ExpenseTable from '../components/partialComponents/ExpenseTable'
+import DataTable from '../components/partialComponents/DataTable'
 
 export default function Expense() {
   const { defaultProjectId } = useSelector((state: RootState) => state.user)
@@ -20,21 +20,21 @@ export default function Expense() {
       <div className="mt-20 p-4 w-full h-full ">
         <div className="flex flex-col items-start justify-center rounded w-full h-full">
           <div className="flex gap-4">
-            <button className="bg-teal-500 text-white  text-2xl p-2 rounded shadow-md cursor-pointer">
+            <button className="bg-white text-teal-500  text-2xl px-4 py-2 rounded-t-lg shadow-md cursor-pointer">
               Weekly Expenses
             </button>
-            <button className="bg-teal-500 text-white text-2xl p-2 rounded shadow-md cursor-pointer">
+            <button className="bg-teal-500 text-white text-2xl text-2xl px-4 py-2 rounded-t-lg shadow-md cursor-pointer">
               Monthly Expenses
             </button>
-            <button className="bg-teal-500 text-white text-2xl p-2 rounded shadow-md cursor-pointer">
+            <button className="bg-teal-500 text-white text-2xl text-2xl px-4 py-2 rounded-t-lg shadow-md cursor-pointer">
               Yearly Expenses
             </button>
-            <button className="bg-teal-500 text-white text-2xl p-2 rounded shadow-md cursor-pointer">
+            <button className="bg-teal-500 text-white text-2xl text-2xl px-4 py-2 rounded-t-lg shadow-md cursor-pointer">
               5 Year Expenses
             </button>
           </div>
           <div className="flex-1 bg-white w-full">
-            <ExpenseTable expenses={expense} />
+            <DataTable data={expense} />
           </div>
         </div>
       </div>
---
+++ outgo-react/src/components/partialComponents/DataTable.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import { Expense } from '../../apis/Expenses/ExpensesRepository'
+import { Income } from '../../apis/Income/IncomeRepository'
+
+interface DataTableProps {
+  data: Expense[] | Income[]
+  itemsPerPage?: number
+}
+
+const DataTable = ({ data, itemsPerPage = 10 }: DataTableProps) => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const totalPages = Math.ceil(data.length / itemsPerPage)
+
+  // Calculate the index range for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const currentItems = data.slice(startIndex, endIndex)
+
+  return (
+    <div className="w-full">
+      {/* Render Items */}
+      <ul className="">
+        {currentItems.map((data, index) => (
+          <li key={index} className="py-4 border-b-2 border-gray-300">
+            <div className="flex justify-between items-center px-4">
+              <span className="flex-1 text-center text-lg border-r-2 border-gray-300 py-2">
+                {data.name}
+              </span>
+              <span className="flex-1 text-center text-lg border-l-2 border-r-2 border-gray-300 py-2">
+                {data.amount}
+              </span>
+              <span className="flex-1 text-center text-lg border-l-2 border-r-2 border-gray-300 py-2">
+                {new Date(data.date).toLocaleDateString('en-GB')}
+              </span>
+              {'category' in data && (
+                <span className="flex-1 text-center text-lg border-l-2 border-r-2 border-gray-300 py-2">
+                  {(data as Expense).category}
+                </span>
+              )}
+              {data.timeframe && (
+                <span className="flex-1 text-center text-lg border-l-2 border-r-2 border-gray-300 py-2">
+                  {data.timeframe}
+                </span>
+              )}
+              {'expenseType' in data && (
+                <span className="flex-1 text-center text-lg border-l-2 border-r-2 border-gray-300 py-2">
+                  {(data as Expense).expenseType}
+                </span>
+              )}
+              {'incomeType' in data && (
+                <span className="flex-1 text-center text-lg border-l-2 border-r-2 border-gray-300 py-2">
+                  {(data as Income).incomeType}
+                </span>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      {/* Pagination Controls */}
+      <div className="flex gap-2 justify-center mt-4">
+        <button
+          className="px-3 py-1 cursor-pointer bg-gray-300 rounded disabled:opacity-50"
+          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+
+        <button
+          className="px-3 py-1  cursor-pointer bg-gray-300 rounded disabled:opacity-50"
+          onClick={() =>
+            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+          }
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  )
+}
+export default DataTable
